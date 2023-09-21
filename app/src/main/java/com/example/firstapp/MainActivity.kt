44356f3b80cecf1cba309ee884ff0ac90b6a14f2
2dam <pabsermat@gmail.com>
2023-09-21 12:01:41 +0200

    Yes

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -15,13 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        editText= this.findViewById(R.id.editText)
+        editText = this.findViewById(R.id.editText)
         buttonOut = this.findViewById(R.id.buttonOut)
         buttonOut.setOnClickListener {
-            if(!editText.text.isNullOrEmpty()){
-            Toast.makeText(this, editText.text, Toast.LENGTH_SHORT).show()
-}else{
-    Toast.makeText(this, "El texto está vacío", Toast.LENGTH_SHORT).show()
+            if (!editText.text.isNullOrEmpty()) {
+                Toast.makeText(this, editText.text, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "El texto está vacío", Toast.LENGTH_SHORT).show()
             }
         }
     }
